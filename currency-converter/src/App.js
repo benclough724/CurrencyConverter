d4c9fd37185cf34c47a5debe56242f94e9cbc931
Ben Clough <ben_clough123@outlook.com>
2021-06-23 15:54:40 +0100

    Tailwind css
    Just trying to get tailwindcss set up with the project

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import './Styles/Tailwind.css'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="grid grid-cols-2">
+      <form class="form-grid">
+        <button type="submit"></button>
+        <button type="submit"></button>  
+        <button type="submit"></button>    
+      </form>
     </div>
   );
 }
