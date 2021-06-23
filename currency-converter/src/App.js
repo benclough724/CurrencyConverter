036fe5f4e1a91b8ae701b36bd8a10c91458b65c0
Ben Clough <ben_clough123@outlook.com>
2021-06-23 19:33:12 +0100

    Form Validation
    Just testing the form validation and its working which wasnt too difficult

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './Styles/Main.css';
 
 function App() {
+
+  const [numToConvert, setNumToConvert] = useState();
+
+  function numToConvertChange(e){
+    e.preventDefault();
+    setNumToConvert(e.target.value);
+  }
+
   return (
     <div className="main-container">
       <form class="calculator">
-        <input type="Text" placeholder="How much you want to convert"/>
+        <input type="Text" placeholder="How much you want to convert"
+        value={numToConvert}
+        onChange={numToConvertChange}
+        onSubmit={numToConvertSubmit}/>
+        <h1>numToConvert: {numToConvert}</h1>
       </form>
     </div>
   );
